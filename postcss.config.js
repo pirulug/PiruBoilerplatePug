3ad87ebb4e95e65fd4e8fd2af408505dfb882f59
Pirulug <pirulug@gmail.com>
2022-09-10 22:10:57 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,3 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-
-process.env.NODE_ENV === "production"
-  ? (module.exports = {
-      plugins: [
-        require("autoprefixer"),
-
-        purgecss({
-          content: ["./dist/*.html"],
-        }),
-      ],
-    })
-  : (module.exports = {
-      plugins: [
-        require("autoprefixer"),
-
-        // purgecss({
-        //   content: ["./dist/*.html"],
-        // }),
-      ],
-    });
-    
+module.exports = {
+  plugins: [require("autoprefixer")],
+};
